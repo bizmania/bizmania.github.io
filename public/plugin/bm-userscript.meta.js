80bba74fe85e1ef6f3a06f06578412fb2bcde32b
Constantine Teplyakov <constnw@gmail.com>
2024-08-05 22:36:23 +0300

    v7.4.8

--- public/plugin/bm-userscript.meta.js
+++ public/plugin/bm-userscript.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name bm-userscript
-// @version 7.4.7
+// @version 7.4.8
 // @namespace https://github.com/bizmania
 // @updateURL https://bizmania.github.io/plugin/bm-userscript.meta.js
 // @downloadURL https://bizmania.github.io/plugin/bm-userscript.user.js
